137106679c1ac4edf11deb656acfc27d5cbec3c1
Kyohei Yamaguchi <kyohei3235@gmail.com>
2024-03-09 18:12:11 +0900

    Reformt code

--- WeatherReport/composeApp/src/commonMain/kotlin/App.kt
+++ WeatherReport/composeApp/src/commonMain/kotlin/App.kt
@@ -1,38 +1,34 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import weatherreport.composeapp.generated.resources.Res
-import weatherreport.composeapp.generated.resources.compose_multiplatform
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        Surface (
+        Surface(
             modifier = Modifier.fillMaxSize()
-        ){
+        ) {
             val scope = rememberCoroutineScope()
             var text by remember { mutableStateOf("Loading") }
-            LaunchedEffect(true){
+            LaunchedEffect(true) {
                 scope.launch {
                     text = try {
                         Greeting().greet()
-                    }catch (e: Exception) {
+                    } catch (e: Exception) {
                         e.message ?: "error"
                     }
                 }
